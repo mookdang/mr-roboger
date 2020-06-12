13f3ba0c62eb49d6dd0feff3cbf7af958de923b3
mook <mookdang24@gmail.com>
2020-06-12 16:29:11 -0700

    fixed readme file

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,17 @@
 $(document).ready(function() { 
   $("form#input").submit(function(event) {
     event.preventDefault();
+
+    let numInput = parseInt($("input#numInput"));
+    let arrayNum = []
+    for (index = 0; index < arrayNum.length; index++) {
+      if (index === 1) {
+        alert = "Beep!"
+      }
+    }
     
-    const numInput = parseInt($("input#numInput").val());
-    if ()
+    //const numInput = parseInt($("input#numInput").val());
+    
     
     $("#output").show();
   });
