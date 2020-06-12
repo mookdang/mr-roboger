ab476f5868a67d6eff7a4315b3819c3cdc928d21
mook <mookdang24@gmail.com>
2020-06-12 15:39:31 -0700

    still working on trying to make the loop work while trying different approch

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 //business logic
-//function userInput(numInput) {
- 
+
 
 //user interface logic
 $(document).ready(function() { 
   $("form#input").submit(function(event) {
     event.preventDefault();
-
+    
     const numInput = parseInt($("input#numInput").val());
-    if (numInput === 1){
-      $("#output").text("Beep!");
-    }
-    else if (numInput === 2){
-      $("#output").text("Boop!");
-    }
-    else if (numInput === 3){
-      $("#output").text("Won't you be my neighbor?");
-    }
+    if ()
+    
+    $("#output").show();
   });
 });
