b927a334fab1b75ff4b1a5d0fbadd6ecea81c385
mook <mookdang24@gmail.com>
2020-06-21 18:49:05 -0700

    trying to get the code to work, but it not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-//business logic
-let userInput = [0,1,2];
-    for (index = 0; index < userInput; index++) {
-      console.log(index);
-    }
-    
-
-//user interface logic
 $(document).ready(function() { 
   $("form#input").submit(function(event) {
     event.preventDefault();
+    
     let numInput = parseInt($("input#numInput").val());
-    const arrayNum = ["Beep", "Boop", ""]
+    let arrayNum = [0,"Beep","Boop","Won't you be my neighbor",4,5,6,7,9];
+    
 
     
+
+/*$(document).ready(function() { 
+  $("form#input").submit(function(event) {
+    event.preventDefault();
     
-    $("#output").show();
+    let numInput = parseInt($("input#numInput").val());
+    let arrayInput = [0,1,2,3,4,5,6,7,9]
+    arrayInput[1] = "Beep"
+    arrayInput[2] = "Boop"
+    arrayInput [3] = "Won't you be my neighbor"
+
+    $("#output").text(result);
   });
 });
+    $("input#number").val("");
+
+
+    const arrayNum = [0,"Beep","Boop","Won't you be my neighbor",4,5,6,7,9];
