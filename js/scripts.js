c8873f8831081ff5b05d97eca30497d59ba09e07
mook <mookdang24@gmail.com>
2020-06-21 20:24:29 -0700

    adjest readme spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,26 @@
 //bussines logic
-function arrayNumber(arrayInput);
-let newArray = []
-for(let index = 0; index <= arrayInput; index++) {
-    newArray.push(index);
-  }     
-  return newArray;
-}
-
-
+function arrayNumber(arrayInput) {
+  let newArray = [];
+  for(let index = 0; index <= arrayInput; index+=1) {
+    if (index.toSpring().includes("3")) { 
+      newArray.push("BEEP!");
+    } else if (index.toSpring().includes("2")) {
+      newArray.push("BOOP!");
+    } else if (index.toSpring().includes("2")) {
+      newArray.push("WON'T YOU BE MY NEIGHBOR!");
+    }
+  }
+    return newArray;
+  }
 
 //user logic
 $(document).ready(function() { 
-  $("form#input").submit(function(event) {
+  $("#inputNumber").submit(function(event) {
     event.preventDefault();
     
-    let InputNum = parseInt($("input#numInput").val());
-    
+    let arrayInput = parseInt($("#numInput").val());
+    let output = arrayNumber(arrayInput);
+   
     $("#output").text(output);
   });
 });
-
-/*$(document).ready(function() { 
-  $("form#input").submit(function(event) {
-    event.preventDefault();
-    
-    let numInput = parseInt($("input#numInput").val());
-    let arrayInput = [0,1,2,3,4,5,6,7,9]
-    arrayInput[1] = "Beep"
-    arrayInput[2] = "Boop"
-    arrayInput [3] = "Won't you be my neighbor"
-
-    $("#output").text(result);
-  });
-});
-    $("input#number").val("");
-
-
-    const arrayNum = [0,"Beep","Boop","Won't you be my neighbor",4,5,6,7,9];
